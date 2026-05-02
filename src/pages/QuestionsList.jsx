561696fd3f12b863e14532b7ac7b90731ddf9580
Pedro Vieira <pedro515vieira@gmail.com>
2026-05-02 01:49:26 -0300

    [feature] - Added more questions info to the questions list

--- src/pages/QuestionsList.jsx
+++ src/pages/QuestionsList.jsx
@@ -30,7 +30,13 @@ const QuestionsList = () => {
             <Card className="h-100">
               <Card.Body>
                 <Card.Text>{e.topic}</Card.Text>
+                <Card.Text>{e.subtopic}</Card.Text>
+                <Card.Text>{e.diversity_mode}</Card.Text>
                 <Card.Title>{e.question}</Card.Title>
+                <Card.Text>A - {e.answer_a}</Card.Text>
+                <Card.Text>B - {e.answer_b}</Card.Text>
+                <Card.Text>C - {e.answer_c}</Card.Text>
+                <Card.Text>D - {e.answer_d}</Card.Text>
               </Card.Body>
             </Card>
           </Col>
