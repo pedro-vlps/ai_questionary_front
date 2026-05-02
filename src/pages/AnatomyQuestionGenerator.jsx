480e82cbe25f96b7a9445596de180f0633b45950
Pedro Vieira <pedro515vieira@gmail.com>
2026-05-01 23:50:03 -0300

    [bugfix] - Changing ai route parameters word

--- src/pages/AnatomyQuestionGenerator.jsx
+++ src/pages/AnatomyQuestionGenerator.jsx
@@ -1,22 +1,32 @@
-import { post } from '../helpers/FecthApi';
-import { useAppContext } from '../helpers/ContextApi';
-import { Card, Col, Row, Spinner } from 'react-bootstrap';
+import { post } from "../helpers/FecthApi";
+import { useAppContext } from "../helpers/ContextApi";
+import { Card, Col, Row, Spinner } from "react-bootstrap";
 
-const anatomyTopics = ['Locomotor', 'Esplacno', 'Neuro'];
+const anatomyTopics = [
+  { title: "Locomotor", apiName: "Locomotor" },
+  { title: "Esplacno", apiName: "Splanchnology" },
+  { title: "Neuro", apiName: "Neuroanatomy" },
+];
 
 const AnatomyQuestionGenerator = () => {
-  const { setQuestionData, resetQuestionState, isLoading, setIsLoading, resetQuestionData } = useAppContext();
+  const {
+    setQuestionData,
+    resetQuestionState,
+    isLoading,
+    setIsLoading,
+    resetQuestionData,
+  } = useAppContext();
 
   const handleButtonClick = async (buttonText) => {
     try {
       setIsLoading(true);
       resetQuestionState();
       resetQuestionData();
-      const response = await post('ai/anatomy', { parameter: buttonText });
-      console.log('Response:', response);
+      const response = await post("ai/anatomy", { parameter: buttonText });
+      console.log("Response:", response);
       setQuestionData(response.data);
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     } finally {
       setIsLoading(false);
     }
@@ -28,25 +38,28 @@ const AnatomyQuestionGenerator = () => {
         <h5>Choose your anatomy topic</h5>
         <Row className="g-3 justify-content-center">
           {anatomyTopics.map((topic) => (
-            <Col key={topic} xs={12} sm={6} md={4} lg={3} className="d-flex">
+            <Col key={topic.title} xs={12} sm={6} md={4} lg={3} className="d-flex">
               <Card
                 className="w-100 h-100"
                 role="button"
-                onClick={() => handleButtonClick(topic)}
+                onClick={() => handleButtonClick(topic.apiName)}
               >
-                <Card.Body>{topic}</Card.Body>
+                <Card.Body>{topic.title}</Card.Body>
               </Card>
             </Col>
           ))}
         </Row>
         {isLoading && (
-          <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '200px' }}>
+          <div
+            className="d-flex justify-content-center align-items-center"
+            style={{ minHeight: "200px" }}
+          >
             <Spinner animation="border" variant="primary" />
           </div>
         )}
       </Col>
     </Row>
-  )
-}
+  );
+};
 
 export default AnatomyQuestionGenerator;
