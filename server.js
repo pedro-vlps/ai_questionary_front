9b1159bdd38ee9e2634a7afa29e1a6b54f145cc5
Pedro Vieira <pedro515vieira@gmail.com>
2026-05-12 19:51:25 -0300

    [bugfix] - Deploy adjustments

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // 📦 Servir o build do React
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
+app.get("/{*path}", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
